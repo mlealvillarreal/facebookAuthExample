8f6c3a064e98839ee9bb5f0a7bdc5c79eff33d90
Mauricio <mauriciolealmx@gmail.com>
2016-10-14 12:20:22 -0500

    Working node + express + facebookAuth app

---
+++ configuration/config.js
@@ -0,0 +1,10 @@
+module.exports={
+  "facebook_api_key" : "585718614946729",
+  "facebook_api_secret" : "fb0437849c5a44028c4769a7243fbb76",
+  "callback_url" : "http://localhost:3000/auth/facebook/callback",
+  "use_database" : "false",
+  "host" : "localhost",
+  "username" : "root",
+  "password" : "",
+  "database" : "DB NAME"
+}
